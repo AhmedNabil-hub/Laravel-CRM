2b989e77b778051c7f92ecf1a6824e3fe3535c68
Ahmed N. EL-Nawawy <a7med.nabil1655@gmail.com>
2021-09-02 03:08:52 +0200

    fix some errors in routes

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,24 @@
 <?php
 
-use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\ProjectController;
 
 
 Auth::routes();
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/home', fn() => redirect()->route('home'));
 
 Route::middleware(['auth'])->group(function () {
   Route::resource('users', UserController::class);
-  Route::resource('clients', UserController::class);
-  Route::resource('projects', UserController::class);
-  Route::resource('tasks', UserController::class);
+  Route::resource('clients', ClientController::class);
+  Route::resource('projects', ProjectController::class);
+  Route::resource('tasks', TaskController::class);
 });
 
 
